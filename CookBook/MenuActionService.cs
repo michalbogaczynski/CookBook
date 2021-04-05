6220660ccb7a452f0be7faad460e08387f26c8a0
michalbogaczynski <42570210+michalbogaczynski@users.noreply.github.com>
2021-04-05 23:16:48 +0200

    Add project files.

---
+++ CookBook/MenuActionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CookBook
+{
+    internal class MenuActionService
+    {
+        internal void addNewAction(int v1, string v2, string v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object GetMenuActionsByMenuName(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CookBook/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CookBook
+{
+    class Program
+    {
+        //Dodawanie przepisów
+        // śniadania / obiady / kolacje / napoje / desery /sałatki
+        // podział na kuchnię włoską / polska itd
+
+        //Wyświetlanie przepisów
+        //Usuwanie przepisów
+
+        static void Main(string[] args)
+        {
+            MenuActionService actionService = new MenuActionService();
+            actionService = Initialize(actionService);
+
+            Console.WriteLine("Witaj w aplikacji książki kucharskiej");
+
+            while (true)
+            {
+                Console.WriteLine("Wybierz akcję:");
+                var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+                Console.WriteLine();
+            }
+           
+        }
+
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.addNewAction(1, "Dodaj przepis", "Main");
+            actionService.addNewAction(2, "Remove recipe", "Main");
+            actionService.addNewAction(3, "Show recipe", "Main");
+
+            actionService.addNewAction(1, "Śniadania", "AddNewRecipe");
+            actionService.addNewAction(2, "Obiady", "AddNewRecipe");
+            actionService.addNewAction(3, "Kolacje", "AddNewRecipe");
+
+            //actionService.addNewAction(1, "Dania kuchani polskiej", "Main");
+            //actionService.addNewAction(1, "Dania kuchni włoskiej", "Main");
+        }
+    }
+}
